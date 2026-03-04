41700774e14d40c0928c1522094d18c8dc3b7b32
ALEX-L1ANG <343293684@tdsb.ca>
2026-03-04 16:13:24 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/*
+Author: Alex Liang
+Lesson: 1.12 Converting Data Types
+Date Created: Mar 4, 2026
+Date Last Modified: Mar 4, 2026
+*/
+
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +18,38 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input an integer: ");
+	int int1 = input.nextInt();
+	int int2 = int1 + 3;
+	System.out.println(int2);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	int num = input.nextInt();
+	double dnum = Double.parseDouble(num + 4);
+	System.out.println(dnum);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a radius: ");
+	double radius = input.nextDouble();
+	double dradius = Double.parseDouble(radius * radius * 3.14);
+	System.out.println(dradius);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num1 = input.nextDouble();
+	double num2 = Double.parseDouble(Math.round(num1 * 12));
+	System.out.println(num2);
 	}
 
 }
